1470b462c862a32543c5c91b41e9b425d179d8e1
Hayashi Hironori <hayaten415@gmail.com>
2019-12-23 21:55:09 +0900

    141 LinkedStyleCycle

---
+++ 141_LinkedListCycle/Solution.java
@@ -0,0 +1,31 @@
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
+ 
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    if (head == null) {
+      return false;
+    }
+
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (slow != null) {
+      if (fast.next == null || fast.next.next == null) {
+        return false;
+      }
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
